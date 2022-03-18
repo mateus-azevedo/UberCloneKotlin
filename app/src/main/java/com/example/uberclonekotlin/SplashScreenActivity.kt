820836422c1bc54965bc00355ca202d6eea88398
Mateus Santos <m.santos.azeve@gmail.com>
2022-03-18 15:04:48 -0300

    configurado listeners do firebase

--- app/src/main/java/com/example/uberclonekotlin/SplashScreenActivity.kt
+++ app/src/main/java/com/example/uberclonekotlin/SplashScreenActivity.kt
@@ -3,20 +3,35 @@ package com.example.uberclonekotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var providers: List<AuthUI.IdpConfig>
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var listener: FirebaseAuth.AuthStateListener
+
+    override fun onStart() {
+        super.onStart()
+        delaySplashScreen()
+    }
+
+    override fun onStop() {
+        if (firebaseAuth != null && listener != null) {
+            firebaseAuth.removeAuthStateListener(listener)
+        }
+        super.onStop()
+    }
+
+    private fun delaySplashScreen() {
         Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
             .subscribe {
-                Toast.makeText(
-                    this@SplashScreenActivity,
-                    "Splash Screen run done",
-                    Toast.LENGTH_SHORT
-                ).show()
+                firebaseAuth.addAuthStateListener(listener)
             }
     }
 }
